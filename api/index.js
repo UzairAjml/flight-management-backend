9ba6868f0c9406255760d9798dee4892fa99b54d
UzairAjml <uzairajmal47@gmail.com>
2023-03-20 15:08:35 +0500

    new changes

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-app.use(cors());
-app.get("/search", async (req, res) => {
-  res.status(200).send("Hello");
-});
