c473537a2460479dfcb71661c4d7f95ce194ae06
uzairajml <uzairajmal47@gmail.com>
2024-07-23 22:34:39 +0500

    test

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var logger = require('morgan');
-var indexRouter = require('./router/index');
-const cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+var logger = require("morgan");
+var indexRouter = require("./router/index");
+const cors = require("cors");
 
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use(cors({
-  origin: '*'
-}));
-app.use('/api', indexRouter);
+
+
+app.use(logger("dev"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use("/api", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- router/index.js
+++ router/index.js
@@ -1,367 +1,78 @@
 var express = require("express");
 var router = express.Router();
-var { DuffelError } = require("@duffel/api");
-var duffel = require("../duffel");
-//search offer API
-router.post("/search", async (req, res) => {
-  const {
-    origin,
-    destination,
-    sort,
-    cabin_class,
-    departure_date,
-    return_date,
-    passengers,
-    return_offer,
-    rangeValues,
-  } = req.body;
-  if (!origin || !destination) {
-    res.sendStatus(422);
-    return;
-  }
-  let slice;
-  try {
-    // create an offer request for a flight departing tomorrow
-    slice = return_offer
-      ? [
-          {
-            origin,
-            destination,
-            departure_date,
-            // departure_time: {
-            //   to: `${
-            //     rangeValues?.from_departure[1] > 9
-            //       ? rangeValues?.from_departure[1]
-            //       : `0${
-            //           isNaN(rangeValues?.from_departure[1])
-            //             ? "0"
-            //             : rangeValues?.from_departure[1]
-            //         }`
-            //   }:00`,
-            //   from: `${
-            //     rangeValues?.from_departure[0] > 9
-            //       ? rangeValues?.from_departure[0]
-            //       : `0${
-            //           isNaN(rangeValues?.from_departure[0])
-            //             ? "0"
-            //             : rangeValues?.from_departure[0]
-            //         }`
-            //   }:00`,
-            // },
-            // arrival_time: {
-            //   to: `${
-            //     rangeValues?.to_departure[1] > 9
-            //       ? rangeValues?.to_departure[1]
-            //       : `0${
-            //           isNaN(rangeValues?.to_departure[1])
-            //             ? "0"
-            //             : rangeValues?.to_departure[1]
-            //         }`
-            //   }:00`,
-            //   from: `${
-            //     rangeValues?.to_departure[0] > 9
-            //       ? rangeValues?.to_departure[0]
-            //       : `0${
-            //           isNaN(rangeValues?.to_departure[0])
-            //             ? "0"
-            //             : rangeValues?.to_departure[0]
-            //         }`
-            //   }:00`,
-            // },
-          },
-          {
-            origin: destination,
-            destination: origin,
-            departure_date: return_date,
-            // departure_time: {
-            //   to: `${
-            //     rangeValues?.from_arrival[1] > 9
-            //       ? rangeValues?.from_arrival[1]
-            //       : `0${
-            //           isNaN(rangeValues?.from_arrival[1])
-            //             ? "0"
-            //             : rangeValues?.from_arrival[1]
-            //         }`
-            //   }:00`,
-            //   from: `${
-            //     rangeValues?.from_arrival[0] > 9
-            //       ? rangeValues?.from_arrival[0]
-            //       : `0${
-            //           isNaN(rangeValues?.from_arrival[0])
-            //             ? "0"
-            //             : rangeValues?.from_arrival[0]
-            //         }`
-            //   }:00`,
-            // },
-            // arrival_time: {
-            //   to: `${
-            //     rangeValues?.to_arrival[1] > 9
-            //       ? rangeValues?.to_arrival[1]
-            //       : `0${
-            //           isNaN(rangeValues?.to_arrival[1])
-            //             ? "0"
-            //             : rangeValues?.to_arrival[1]
-            //         }`
-            //   }:00`,
-            //   from: `${
-            //     rangeValues?.to_arrival[0] > 9
-            //       ? rangeValues?.to_arrival[0]
-            //       : `0${
-            //           isNaN(rangeValues?.to_arrival[0])
-            //             ? "0"
-            //             : rangeValues?.to_arrival[0]
-            //         }`
-            //   }:00`,
-            // },
-          },
-        ]
-      : [
-          {
-            origin,
-            destination,
-            departure_date,
-            // departure_time: {
-            //   to: `${
-            //     rangeValues?.from_departure[1] > 9
-            //       ? rangeValues?.from_departure[1]
-            //       : `0${
-            //           isNaN(rangeValues?.from_departure[1])
-            //             ? "0"
-            //             : rangeValues?.from_departure[1]
-            //         }`
-            //   }:00`,
-            //   from: `${
-            //     rangeValues?.from_departure[0] > 9
-            //       ? rangeValues?.from_departure[0]
-            //       : `0${
-            //           isNaN(rangeValues?.from_departure[0])
-            //             ? "0"
-            //             : rangeValues?.from_departure[0]
-            //         }`
-            //   }:00`,
-            // },
-            // arrival_time: {
-            //   to: `${
-            //     rangeValues?.to_departure[1] > 9
-            //       ? rangeValues?.to_departure[1]
-            //       : `0${
-            //           isNaN(rangeValues?.to_departure[1])
-            //             ? "0"
-            //             : rangeValues?.to_departure[1]
-            //         }`
-            //   }:00`,
-            //   from: `${
-            //     rangeValues?.to_departure[0] > 9
-            //       ? rangeValues?.to_departure[0]
-            //       : `0${
-            //           isNaN(rangeValues?.to_departure[0])
-            //             ? "0"
-            //             : rangeValues?.to_departure[0]
-            //         }`
-            //   }:00`,
-            // },
-          },
-        ];
+const nodemailer = require('nodemailer');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const csv = require('csv-parser');
+const multer = require('multer');
+const path = require('path');
 
-    const offerRequestsResponse = await duffel.offerRequests.create({
-      slices: [...slice],
-      passengers: [...passengers],
-      cabin_class,
-      max_connections: 2,
-      // requestedSources:["duffel_airways"],
-      return_offers: !return_offer,
-    });
-    res.send({
-      offer: offerRequestsResponse.data,
-    });
-  } catch (e) {
-    console.error(e);
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors, data: slice });
-      return;
-    }
-    res.status(500).send(e);
-  }
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.email',
+  secure: false,
+  service: 'gmail',
+  auth: {
+    user: 'abdullahsaigal@gensols.org',
+    pass: 'yxvh vcoc zbte eida'
+  },
 });
 
-//List Airlines
+const upload = multer({ dest: 'uploads/' });
 
-router.get("/getAirlines", async (req, res) => {
+const readCSV = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const recipients = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on('data', (data) => recipients.push(data.email)) // Assumes the CSV has a column named 'email'
+      .on('end', () => resolve(recipients))
+      .on('error', (error) => reject(error));
+  });
+};
+
+const formatMessage = (message) => {
+  return message
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/\n/g, '<br>')
+    .replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;')
+    .replace(/ {2}/g, '&nbsp;&nbsp;')
+    .replace(/ {1}/g, '&nbsp;');
+};
+
+router.post('/upload', upload.single('file'), async (req, res) => {
   try {
-    const airlines = await duffel.airlines.list({
-      limit: 50,
+    const filePath = req.file.path;
+    const recipients = await readCSV(filePath);
+    const { message } = req.body;
+
+    const source = fs.readFileSync(path.join(__dirname, 'new-email.html'), 'utf-8').toString();
+    const template = handlebars.compile(source);
+    const replacements = {
+      message: formatMessage(message),
+    };
+    const htmlToSend = template(replacements);
+
+    const emailPromises = recipients.map((recipient) => {
+      return transporter.sendMail({
+        from: 'abdullahsaigal@gensols.org',
+        to: recipient,
+        subject: 'Welcome to Gensols',
+        text: message, // Plain text version
+        html: htmlToSend, // HTML version
+      });
     });
 
-    res.send({
-      offer: airlines,
-    });
-  } catch (e) {
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors });
-      return;
-    }
-    res.status(500).send(e);
-  }
-});
+    const info = await Promise.all(emailPromises);
 
-//List offer API
-router.get("/getOffers/:id", async (req, res) => {
-  if (!req.params["id"]) {
-    res.sendStatus(422);
-    return;
-  }
-  try {
-    const payload = req.query?.after
-      ? {
-          offer_request_id: req.params["id"],
-          sort: "total_amount",
-          limit: 20,
-        }
-      : req.query?.before
-      ? {
-          offer_request_id: req.params["id"],
-          sort: "total_amount",
-          limit: 20,
-          before: req.query?.before,
-        }
-      : {
-          offer_request_id: req.params["id"],
-          sort: "total_amount",
-          limit: 20,
-          after: req.query?.after,
-        };
-        console.log("payload",payload)
-    const offersResponse = await duffel.offers.list(payload);
-
-    res.send({
-      offer: offersResponse,
-    });
-  } catch (e) {
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors });
-      return;
-    }
-    res.status(500).send(e);
-  }
-});
-// seatPlan API
-router.get("/getSeatPlan/:id", async (req, res) => {
-  if (!req.params["id"]) {
-    res.sendStatus(422);
-    return;
-  }
-  try {
-    const offersResponse = await duffel.seatMaps.get({
-      offer_id: req.params["id"],
-    });
-    res.send({
-      offer: offersResponse.data,
-    });
-  } catch (e) {
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors });
-      return;
-    }
-    res.status(500).send(e);
-  }
-});
-//single offer details
-router.get("/getSingleOffer/:id", async (req, res) => {
-  if (!req.params["id"]) {
-    res.sendStatus(422);
-    return;
-  }
-  try {
-    const offersResponse = await duffel.offers.get(req.params["id"]);
-    res.send({
-      offer: offersResponse,
-    });
-  } catch (e) {
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors });
-      return;
-    }
-    res.status(500).send(e);
-  }
-});
-//search places api
-router.get("/searchPlace/:query", async (req, res) => {
-  if (!req.params["query"]) {
-    res.sendStatus(422);
-    return;
-  }
-  try {
-    const offersResponse = await duffel.suggestions.list({
-      query: req.params["query"],
-    });
-    res.send({
-      offer: offersResponse,
-    });
-  } catch (e) {
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors });
-      return;
-    }
-    res.status(500).send(e);
-  }
-});
-//create payment intent
-router.post("/paymentIntent", async (req, res) => {
-  const { total_amount, total_currency } = req.body;
-  try {
-    const offersResponse = await duffel.paymentIntents.create({
-      currency: total_currency,
-      amount: total_amount,
-    });
-    res.send({
-      offer: offersResponse,
-    });
-  } catch (e) {
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors });
-      return;
-    }
-    res.status(500).send(e);
-  }
-});
-
-//confirm payment
-router.get("/confirm-payment/:id", async (req, res) => {
-  if (!req.params["id"]) {
-    res.sendStatus(422);
-    return;
-  }
-  try {
-    const offersResponse = await duffel.paymentIntents.confirm(
-      req.params["id"]
-    );
-    res.send({
-      offer: offersResponse,
-    });
-  } catch (e) {
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors });
-      return;
-    }
-    res.status(500).send(e);
-  }
-});
-
-//create order
-router.post("/create-order", async (req, res) => {
-  try {
-    const offersResponse = await duffel.orders.create({
-      ...req.body,
-    });
-    res.send({
-      offer: offersResponse,
-    });
-  } catch (e) {
-    if (e instanceof DuffelError) {
-      res.status(e.meta.status).send({ errors: e.errors });
-      return;
-    }
-    res.status(500).send(e);
+    console.log('Messages sent: %s', info.map((i) => i.response).join(', '));
+    res.send('Emails Sent!');
+  } catch (error) {
+    console.error('Error sending emails:', error);
+    res.status(500).send('An error occurred while sending emails');
+  } finally {
+    fs.unlinkSync(req.file.path); // Delete the uploaded file
   }
 });
 
