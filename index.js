ca29b3e08e485029650c26b7a8d7f0fca430ba1c
uzairajml <uzairajmal47@gmail.com>
2024-08-11 21:34:02 +0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var createError = require("http-errors");
-var express = require("express");
-var logger = require("morgan");
-var indexRouter = require("./router/index");
-const cors = require("cors");
+var createError = require('http-errors');
+var express = require('express');
+var logger = require('morgan');
+var indexRouter = require('./router/index');
+const cors = require('cors');
 
 var app = express();
 
@@ -13,7 +13,6 @@ app.use(cors({
   origin: '*'
 }));
 app.use('/api', indexRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -27,8 +26,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5001);
 
 module.exports = app;
