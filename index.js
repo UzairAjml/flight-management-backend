57ba3aa0e1d94bd482aa13ccbd1742e30717e7d3
uzairajml <uzairajmal47@gmail.com>
2024-08-11 21:18:56 +0500

    reverted

--- index.js
+++ index.js
@@ -8,16 +8,12 @@ var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(cors({
+  origin: '*'
+}));
+app.use('/api', indexRouter);
 
-
-app.use(logger("dev"));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.use("/api", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
